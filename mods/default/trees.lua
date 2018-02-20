4fd2ef52f84ae36c0e3c74388a44f13401233ddf
minduser00 <minduser00@users.noreply.github.com>
2018-02-20 15:23:33 +0000

    Suppressing unnecessary local variables

--- mods/default/trees.lua
+++ mods/default/trees.lua
@@ -8,19 +8,13 @@ local random = math.random
 
 function default.can_grow(pos)
 	local node_under = minetest.get_node_or_nil({x = pos.x, y = pos.y - 1, z = pos.z})
-	if not node_under then
-		return false
+	if node_under and minetest.get_item_group(node_under.name, "soil") ~= 0 then
+		local light_level = minetest.get_node_light(pos)
+		if light_level and light_level > 12 then
+			return true
+		end
 	end
-	local name_under = node_under.name
-	local is_soil = minetest.get_item_group(name_under, "soil")
-	if is_soil == 0 then
-		return false
-	end
-	local light_level = minetest.get_node_light(pos)
-	if not light_level or light_level < 13 then
-		return false
-	end
-	return true
+	return false
 end
 
 
